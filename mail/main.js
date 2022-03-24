9daaa837366ac585ecc064285fabf9306d59c520
Lorenzo <lorenzo.miccio97@gmail.com>
2022-03-24 14:52:18 +0100

    Completato bonus Mail

--- mail/main.js
+++ mail/main.js
@@ -1,8 +1,9 @@
 //Input Utente
 let emailUtente = prompt("Inserisci la tua e-mail");
-emailUtente = emailUtente.toLocaleLowerCase();
+emailUtente = emailUtente.toLowerCase();
 //Lista
 let listaEmail = ["matteo@gmail.com", "fabio@gmail.com", "pietro@gmail.com", "anna@gmail.com"];
+let aggiunta = "si";
 //Controllo
 let checkPresenza = false;
 for(i = 0; i < listaEmail.length; i++){
@@ -14,5 +15,16 @@ for(i = 0; i < listaEmail.length; i++){
 if(checkPresenza){
     alert("Sei presente nella lista di chi può accedere!")
 } else {
-    alert("Non sei presente nella lista di chi può accedere...")
+    aggiunta = prompt("Non sei presente nella lista di chi può accedere, vuoi essere aggiunto? (scrivi \"si\" o \"no\")")
+    aggiunta = aggiunta.toLowerCase();
+    console.log(aggiunta)
+    if(aggiunta == "si"){
+        listaEmail.push(emailUtente);
+        alert("Sei stato aggiunto correttamente alla lista!");
+        console.log(listaEmail);
+    } else if (aggiunta == "no"){
+        alert("Non sei stato aggiunto alla lista");
+    } else {
+        alert("Non hai scelto si o no, non verrai aggiunto alla lista");
+    }
 }
